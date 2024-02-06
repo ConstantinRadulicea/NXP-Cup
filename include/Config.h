7e2c48a61d5596c7193c2b9cb84025fb83fe227d
Badboy <constantinradulicea@gmail.com>
2024-02-06 14:36:35 +0200

    updates

--- include/Config.h
+++ include/Config.h
@@ -1,5 +1,8 @@
 //All defines and Includes
 
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
 #define ENABLE_ARDUINO 0
 
 #include <Arduino.h>
@@ -18,10 +21,9 @@
 
 
 
-// Cod redundant
-//#define ENABLE_SERIAL_PRINT 1     
-//#define ENABLE_STEERING_SERVO 1
-//#define ENABLE_DRIVERMOTOR 1
+#define ENABLE_SERIAL_PRINT 1     
+#define ENABLE_STEERING_SERVO 1
+#define ENABLE_DRIVERMOTOR 1
 #define ENABLE_PIXY_VECTOR_APPROXIMATION 1
 #define ENABLE_WIRELESS_DEBUG 1
 
@@ -78,6 +80,8 @@
 
 #define VECTOR_UNIT_PER_CM (float)((float)LANE_WIDTH_VECTOR_UNIT_REAL / (float)LANE_WIDTH_CM)   // CM * VECTOR_UNIT_PER_CM = VECTOR_UNIT
 #define CM_PER_VECTOR_UNIT (float)((float)LANE_WIDTH_CM / (float)LANE_WIDTH_VECTOR_UNIT_REAL)   // VECTOR_UNIT_PER_CM * CM = CM
+#define RADIANS_PER_DEGREE (float)((float)M_PI / 180.0f) // DEGREE * RADIAN_PER_DEGREE = RADIAN
+#define DEGREES_PER_RADIAN (float)(180.0f / (float)M_PI) // RADIAN * DEGREE_PER_RADIAN = DEGREE
 
 #define LANE_WIDTH_VECTOR_UNIT (float)(LANE_WIDTH_VECTOR_UNIT_REAL + ((5.0f * VECTOR_UNIT_PER_CM)*2.0f))
 
@@ -90,8 +94,9 @@
 #define MAX_SPEED (int)100
 #define STANDSTILL_SPEED (int)90
 
+/*====================================================================================================================================*/
 
-void printDataToSerial(Vector leftVectorOld, Vector rightVectorOld, Vector leftVector, Vector rightVector, LineABC leftLine, LineABC rightLine, LineABC laneMiddleLine, PurePersuitInfo purePersuitInfo, float carAcceleration){
+static void printDataToSerial(Vector leftVectorOld, Vector rightVectorOld, Vector leftVector, Vector rightVector, LineABC leftLine, LineABC rightLine, LineABC laneMiddleLine, PurePersuitInfo purePersuitInfo, float carAcceleration){
   SERIAL.print(String(leftVectorOld.m_x0) + String(',') + String(leftVectorOld.m_y0) + String(',') + String(leftVectorOld.m_x1) + String(',') + String(leftVectorOld.m_y1));
   SERIAL.print(';');
   SERIAL.print(String(rightVectorOld.m_x0) + String(',') + String(rightVectorOld.m_y0) + String(',') + String(rightVectorOld.m_x1) + String(',') + String(rightVectorOld.m_y1));
@@ -115,4 +120,28 @@ void printDataToSerial(Vector leftVectorOld, Vector rightVectorOld, Vector leftV
   SERIAL.print(String(carAcceleration));
   SERIAL.println();
 }
+
+/*==============================================================================*/
+
+static void serial2WifiConnect(HardwareSerial &serialPort, String initSequence, String wifiSsid, String wifiPassword, String hostname, int port){
+  String commentChar = String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING);
+  serialPort.print(initSequence);
+  serialPort.println(commentChar + wifiSsid);
+  serialPort.println(commentChar + wifiPassword);
+  serialPort.println(commentChar + hostname);
+  serialPort.println(commentChar + String(port));
+}
+
+/*==============================================================================*/
+
+static void printSerial2WifiInfo(String initSequence, String wifiSsid, String wifiPassword, String hostname, int port){
+  String commentChar = String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING);
+  SERIAL.print(commentChar + String("WIFI INIT SEQUENCE: ") + initSequence);
+  SERIAL.println(commentChar + String("SSID: ") + wifiSsid);
+  SERIAL.println(commentChar + String("PASSWORD: ") + wifiPassword);
+  SERIAL.println(commentChar + String("HOSTNAME: ") + hostname);
+  SERIAL.println(commentChar + String("PORT: ") + String(port));
+}
+
+#endif
 
--- src/main_v3_PixyVectors.cpp
+++ src/main_v3_PixyVectors.cpp
@@ -2,6 +2,8 @@
 
 #include "Config.h"
 
+
+
 SteeringWheel steeringWheel(STEERING_SERVO_ANGLE_MAX_LEFT, STEERING_SERVO_ANGLE_MIDDLE, STEERING_SERVO_ANGLE_MAX_RIGHT, (unsigned int)0);
 
 #if ENABLE_ARDUINO == 1
@@ -10,76 +12,18 @@ SteeringWheel steeringWheel(STEERING_SERVO_ANGLE_MAX_LEFT, STEERING_SERVO_ANGLE_
   PWMServo driverMotor;
 #endif
 
-
 VectorsProcessing vectorsProcessing;
 Pixy2 pixy;
-int8_t res;
-  
 
-void setup() {
-    // serial Initialization
-    #if ENABLE_SERIAL_PRINT == 1 || ENABLE_WIRELESS_DEBUG == 1
-      SERIAL.begin(115200);
-      while (!SERIAL){
-        delay(100);
-      }
-    #endif
-
-    #if ENABLE_WIRELESS_DEBUG == 1
-      SERIAL.print(DEBUG_WIFI_INIT_SEQUENCE);
-      SERIAL.println(String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING) + String(DEBUG_WIFI_SSID));
-      SERIAL.println(String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING) + String(DEBUG_WIFI_PASSWORD));
-      SERIAL.println(String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING) + String(DEBUG_HOST_IPADDRESS));
-      SERIAL.println(String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING) + String(DEBUG_HOST_PORT));
-    #endif
-
-    #if ENABLE_WIRELESS_DEBUG == 1
-      SERIAL.println(String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING) + String(DEBUG_HOST_IPADDRESS));
-    #endif
-
-    // Initialization and attachment of the servo and motor
-    
-    #if ENABLE_STEERING_SERVO == 1
-      steeringWheel.attach(STEERING_SERVO_PIN);
-      steeringWheel.setSteeringAngleDeg(0);
-    #endif
-
-    #if ENABLE_DRIVERMOTOR == 1
-      #if ENABLE_ARDUINO == 1
-        driverMotor.attach(DRIVER_MOTOR_PIN);
-        driverMotor.writeMicroseconds(1500);
-      #else
-        driverMotor.attach(DRIVER_MOTOR_PIN, 1000, 2000);
-      #endif      
-      driverMotor.write(STANDSTILL_SPEED);
-    #endif
-    
-    // we must initialize the pixy object
-    res = pixy.init();
-    #if ENABLE_SERIAL_PRINT == 1
-      SERIAL.println(String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING) + String("pixy.init() = ") + String(res));
-    #endif
-    
-    // Getting the RGB pixel values requires the 'video' program
-    res = pixy.changeProg("line");
-    #if ENABLE_SERIAL_PRINT == 1
-      SERIAL.println(String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING) + String("pixy.changeProg(line) = ") + String(res));
-    #endif
-    #if ENABLE_DRIVERMOTOR == 1
-      delay(3000);
-    #endif
-    #if ENABLE_SERIAL_PRINT == 1
-      SERIAL.println(String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING) + String("Setup completed!"));
-    #endif
-}
+/*====================================================================================================================================*/
 
 void FailureModeMessage(Pixy2 &pixy, int iteration, String errorText, float &carSpeed){
   #if ENABLE_SERIAL_PRINT == 1
-    SERIAL.println(String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING) + String(" ") + String(iteration) + String(" errorText"));
+    SERIAL.println(String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING) + String("iters [") + String(iteration) + String("] " + errorText));
   #endif
   if (iteration >= 5){  
     #if ENABLE_DRIVERMOTOR == 1
-      driverMotor.write(STANDSTILL_SPEED);
+      driverMotor.write((int)STANDSTILL_SPEED);
     #endif
     carSpeed = (float)STANDSTILL_SPEED;
     while (pixy.init() != PIXY_RESULT_OK) {
@@ -88,9 +32,66 @@ void FailureModeMessage(Pixy2 &pixy, int iteration, String errorText, float &car
   }
 }
 
+/*==============================================================================*/
+
+void setup() {
+  int8_t pixyResult;
+
+  // serial Initialization
+  #if ENABLE_SERIAL_PRINT == 1 || ENABLE_WIRELESS_DEBUG == 1
+    SERIAL.begin(115200);
+    while (!SERIAL){
+      delay(100);
+    }
+  #endif
+
+  #if ENABLE_WIRELESS_DEBUG == 1
+    serial2WifiConnect(SERIAL, String(DEBUG_WIFI_INIT_SEQUENCE), String(DEBUG_WIFI_SSID), String(DEBUG_WIFI_PASSWORD), String(DEBUG_HOST_IPADDRESS), DEBUG_HOST_PORT);
+  #endif
+
+  #if ENABLE_WIRELESS_DEBUG == 1 && ENABLE_SERIAL_PRINT == 1
+    printSerial2WifiInfo(String(DEBUG_WIFI_INIT_SEQUENCE), String(DEBUG_WIFI_SSID), String(DEBUG_WIFI_PASSWORD), String(DEBUG_HOST_IPADDRESS), DEBUG_HOST_PORT);
+  #endif
+
+  // Initialization and attachment of the servo and motor
+  #if ENABLE_STEERING_SERVO == 1
+    steeringWheel.attach(STEERING_SERVO_PIN);
+    steeringWheel.setSteeringAngleDeg(0);
+  #endif
+
+  #if ENABLE_DRIVERMOTOR == 1
+    #if ENABLE_ARDUINO == 1
+      driverMotor.attach(DRIVER_MOTOR_PIN);
+      driverMotor.writeMicroseconds(1500);
+    #else
+      driverMotor.attach(DRIVER_MOTOR_PIN, 1000, 2000);
+    #endif
+    driverMotor.write((int)STANDSTILL_SPEED);
+  #endif
+    
+  // we must initialize the pixy object
+  pixyResult = pixy.init();
+  #if ENABLE_SERIAL_PRINT == 1
+    SERIAL.println(String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING) + String("pixy.init() = ") + String(pixyResult));
+  #endif
+    
+  // Getting the RGB pixel values requires the 'video' program
+  pixyResult = pixy.changeProg("line");
+  #if ENABLE_SERIAL_PRINT == 1
+    SERIAL.println(String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING) + String("pixy.changeProg(line) = ") + String(pixyResult));
+  #endif
+  #if ENABLE_DRIVERMOTOR == 1
+    delay(3000);
+  #endif
+  #if ENABLE_SERIAL_PRINT == 1
+    SERIAL.println(String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING) + String("Setup completed!"));
+  #endif
+}
+
+/*====================================================================================================================================*/
 void loop() {
-  int i, loop_iter_timeout_vector = 0;
-  uint32_t timeStart, noVectorDetectedIterationCount;;
+  int i;
+  uint32_t timeStart, loopIterationsCountNoVectorDetected, loopIterationsCountVectorRetriveError, loopIterationsCountPixyChangeProgramError;
   LineABC laneMiddleLine, mirrorLine;
   Vector vec, leftVectorOld, rightVectorOld;
   PurePersuitInfo purePersuitInfo;
@@ -99,7 +100,8 @@ void loop() {
   
   carSpeed = 0.0f;
   timeStart = 0;
-  noVectorDetectedIterationCount = 0;
+  loopIterationsCountNoVectorDetected = 0;
+  loopIterationsCountVectorRetriveError = 0;
 
   mirrorLine = xAxisABC();
   mirrorLine.C = -(((float)IMAGE_MAX_Y) / 2.0f);
@@ -114,18 +116,18 @@ void loop() {
   
   vectorsProcessing.setCarPosition(carPosition);
   vectorsProcessing.setLaneWidth(laneWidth);
-  vectorsProcessing.setMinXaxisAngle((3.0f / 180.0f) * M_PI);
+  vectorsProcessing.setMinXaxisAngle(3.0f * RADIANS_PER_DEGREE);
   while (1)
   {
     timeStart = millis();
     vectorsProcessing.clear();
     if(pixy.line.getAllFeatures(LINE_VECTOR) >= (int8_t)0){
-      loop_iter_timeout_vector = 0;
+      loopIterationsCountVectorRetriveError = 0;
       if (pixy.line.numVectors > 0){
-        noVectorDetectedIterationCount = 0;
+        loopIterationsCountNoVectorDetected = 0;
       }
       else{
-        noVectorDetectedIterationCount++;
+        loopIterationsCountNoVectorDetected++;
       }
       
       for (i=0; i < pixy.line.numVectors; i++)
@@ -141,13 +143,15 @@ void loop() {
       #if ENABLE_PIXY_VECTOR_APPROXIMATION == 1
       if (((int)vectorsProcessing.isVectorValid(rightVectorOld) + (int)vectorsProcessing.isVectorValid(leftVectorOld))==1){
         carSpeed = (float)MIN_SPEED;
-        i=0;
+
+        loopIterationsCountPixyChangeProgramError=0;
         while (pixy.changeProg("video") != PIXY_RESULT_OK)
         {
-          i++;
-          FailureModeMessage(pixy, i,"ERROR: pixy.changeProg(\"video\")",carSpeed);
+          loopIterationsCountPixyChangeProgramError++;
+          FailureModeMessage(pixy, loopIterationsCountPixyChangeProgramError,"ERROR: pixy.changeProg(\"video\")",carSpeed);
         }
         delay(40);
+
         vec = VectorsProcessing::mirrorVector(mirrorLine, leftVectorOld);
         approximatePixyVectorVector(pixy, vec, BLACK_COLOR_TRESHOLD, mirrorImage(mirrorLine, carPosition));
         vec = VectorsProcessing::mirrorVector(mirrorLine, vec);
@@ -158,9 +162,10 @@ void loop() {
         vec = VectorsProcessing::mirrorVector(mirrorLine, vec);
         vectorsProcessing.setRightVector(vec);
         
+        loopIterationsCountPixyChangeProgramError = 0;
         while (pixy.changeProg("line") != PIXY_RESULT_OK) {
-          i++;
-          FailureModeMessage(pixy, i,"ERROR: pixy.changeProg(\"line\")",carSpeed);
+          loopIterationsCountPixyChangeProgramError++;
+          FailureModeMessage(pixy, loopIterationsCountPixyChangeProgramError,"ERROR: pixy.changeProg(\"line\")",carSpeed);
         }
         delay(40);
       }
@@ -169,23 +174,21 @@ void loop() {
       laneMiddleLine = vectorsProcessing.getMiddleLine();
       purePersuitInfo = purePursuitComputeABC(carPosition, laneMiddleLine, carLength, lookAheadDistance);
 
-      if (noVectorDetectedIterationCount > 15)
+      if (loopIterationsCountNoVectorDetected > 15)
       {
         carSpeed = (float)MIN_SPEED;
         #if ENABLE_DRIVERMOTOR == 1
-          driverMotor.write(carSpeed);
+          driverMotor.write((int)carSpeed);
         #endif
       }
       else{
-        carSpeed = MIN((abs((float)STEERING_SERVO_MAX_ANGLE - (float)abs(purePersuitInfo.steeringAngle * (180.0f / M_PI))) / (float)STEERING_SERVO_MAX_ANGLE) * (float)(MAX_SPEED - STANDSTILL_SPEED), (float)MAX_SPEED) + (float)STANDSTILL_SPEED;
+        carSpeed = MIN((abs((float)STEERING_SERVO_MAX_ANGLE - (float)abs(purePersuitInfo.steeringAngle * DEGREES_PER_RADIAN)) / (float)STEERING_SERVO_MAX_ANGLE) * (float)(MAX_SPEED - STANDSTILL_SPEED), (float)MAX_SPEED) + (float)STANDSTILL_SPEED;
         carSpeed = MAX((float)carSpeed, (float)MIN_SPEED);
       }
-      
-
     }
     else{
-      loop_iter_timeout_vector++;
-      FailureModeMessage(pixy, loop_iter_timeout_vector,"ERROR: pixy.line.getAllFeatures(LINE_VECTOR)",carSpeed);
+      loopIterationsCountVectorRetriveError++;
+      FailureModeMessage(pixy, loopIterationsCountVectorRetriveError,"ERROR: pixy.line.getAllFeatures(LINE_VECTOR)",carSpeed);
     }
     
     #if ENABLE_SERIAL_PRINT == 1
@@ -193,7 +196,7 @@ void loop() {
     #endif
     
     #if ENABLE_STEERING_SERVO == 1
-      steeringWheel.setSteeringAngleDeg(purePersuitInfo.steeringAngle * (180.0f / M_PI));
+      steeringWheel.setSteeringAngleDeg(purePersuitInfo.steeringAngle * DEGREES_PER_RADIAN);
     #endif
 
     #if ENABLE_DRIVERMOTOR == 1
@@ -201,7 +204,7 @@ void loop() {
     #endif
     
     #if ENABLE_SERIAL_PRINT == 1
-      SERIAL.println(String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING) + String(" LoopTime: ") + String(millis() - timeStart) + String(" ms"));
+      SERIAL.println(String(ESCAPED_CHARACTER_AT_BEGINNING_OF_STRING) + String("LoopTime: ") + String(millis() - timeStart) + String(" ms"));
     #endif
   }
 }
