66c5daf373f6ebd642804cac8451edf97c9f1f35
Constantin Dumitru Petre RADULICEA <constantinradulicea@gmail.com>
2024-05-29 11:56:32 +0300

    updates

--- include/Config.h
+++ include/Config.h
@@ -60,11 +60,12 @@ steering_wheel_angle_offset = 0.0;
 #endif
 
 
-#define DEBUG_MODE 1
-#define RACE_MODE 0
+#define DEBUG_MODE 0
+#define RACE_MODE 1
 #define TEMP_MODE 0
 
 /*====================================================================================================================================*/
+#if CAR1 == 1
 static int enable_car_engine = 0;
 static int enable_car_steering_wheel = 0;
 static int enable_emergency_brake = 1;
@@ -78,15 +79,15 @@ static float lane_width_vector_unit_real = 53.0f;
 static float black_color_treshold = 0.2f; // 0=black, 1=white
 static float car_length_cm = 17.5f;
 static float lookahead_min_distance_cm = 22.0f;
-static float lookahead_max_distance_cm = 45.0f;
+static float lookahead_max_distance_cm = 55.0f;
 static float min_speed = 97.0f + CAR2_PARAMETERS_DIFFERENCE;
-static float max_speed = 128.0f  + CAR2_PARAMETERS_DIFFERENCE;
+static float max_speed = 135.0f  + CAR2_PARAMETERS_DIFFERENCE;
 static float emergency_break_distance_cm = 75.0f;
 static float emergency_brake_min_speed = 94.0f + CAR2_PARAMETERS_DIFFERENCE;
 static float emergency_brake_distance_from_obstacle_cm = 9.0f;   // 13.5f
 static float steering_wheel_angle_offset = 0.0f;
 static float min_axis_angle_vector = 15.0f;
-static float max_speed_after_emergency_brake_delay = 105.0f;
+static float max_speed_after_emergency_brake_delay = 107.0f;
 static float car_speed_ki = -0.02f;
 static float car_speed_kd = -0.2f;
 static float car_speed_ki_min_max_impact = 5.0f;
@@ -99,7 +100,43 @@ static float car_speed_ki_min_max_impact = 5.0f;
 #else
   static float emergency_brake_enable_delay_s = 15.0f;
 #endif
+#elif CAR2 == 1
+static int enable_car_engine = 0;
+static int enable_car_steering_wheel = 0;
+static int enable_emergency_brake = 1;
+static int enable_pixy_vector_approximation_soft = 0;
+static int enable_distance_sensor1_soft = 1;
+static int enable_distance_sensor2_soft = 1;
+static int enable_distance_sensor3_soft = 1;
+static int enable_remote_start_stop_soft = 0;
 
+static float lane_width_vector_unit_real = 53.0f;
+static float black_color_treshold = 0.2f; // 0=black, 1=white
+static float car_length_cm = 17.5f;
+static float lookahead_min_distance_cm = 22.0f;
+static float lookahead_max_distance_cm = 50.0f;
+static float min_speed = 97.0f + CAR2_PARAMETERS_DIFFERENCE;
+static float max_speed = 125.0f  + CAR2_PARAMETERS_DIFFERENCE;
+static float emergency_break_distance_cm = 75.0f;
+static float emergency_brake_min_speed = 94.0f + CAR2_PARAMETERS_DIFFERENCE;
+static float emergency_brake_distance_from_obstacle_cm = 9.0f;   // 13.5f
+static float steering_wheel_angle_offset = 0.0f;
+static float min_axis_angle_vector = 15.0f;
+static float max_speed_after_emergency_brake_delay = 107.0f;
+static float car_speed_ki = -0.01f;
+static float car_speed_kd = -0.4f;
+static float car_speed_ki_min_max_impact = 5.0f;
+
+
+#if RACE_MODE == 1
+  static float emergency_brake_enable_delay_s = 20.0f;
+#elif DEBUG_MODE == 1
+  static float emergency_brake_enable_delay_s = 0.0f;
+#else
+  static float emergency_brake_enable_delay_s = 15.0f;
+#endif
+
+#endif
 
 
 /*====================================================================================================================================*/
