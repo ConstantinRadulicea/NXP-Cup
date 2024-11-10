35229d015a76fd9a39df41388fa792c75491f131
Constantin RADULICEA <constantinradulicea@gmail.com>
2024-11-10 22:40:53 +0200

    updates

---
+++ Unitest/pixy_usb_demo/pixy2_libs/host/libpixyusb2/src/util.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <time.h>
+#include "util.h"
+
+
+uint32_t millis()
+{
+  uint64_t c = clock();
+  c *= 1000;
+  c /= CLOCKS_PER_SEC;
+  return c;
+}
+
+void delayMicroseconds(uint32_t us)
+{
+  // Called from TPixy2 class --  not needed because we are using USB not serial
+}
+
+Console Serial;
+
+
+void Console::print(const char *msg)
+{
+  printf("%s", msg);
+}
+
+void Console::println(const char *msg)
+{
+  printf("%s\n", msg);
+}
+
+
