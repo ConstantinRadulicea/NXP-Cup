7524b7276c8515e661dacf984b9b8c2e7dbad6c6
Constantin RADULICEA <constantinradulicea@gmail.com>
2025-05-01 00:46:21 +0300

    updates

--- matlab/sendData.m
+++ matlab/sendData.m
@@ -18,7 +18,7 @@ g_black_color_treshold = 0.2;
 g_lookahead_min_distance_cm = 20.0;                       % 22
 g_lookahead_max_distance_cm = 50.0;                       % 40
 g_vehicle_min_speed_mps = 0.5;
-g_vehicle_max_speed_mps = 3.5;                                      % 
+g_vehicle_max_speed_mps = 2.6;                                      % 
 g_car_speed_mps_ki = -0.001;                                      %-0.02;
 g_car_speed_mps_kd = 0.0;                                      %-0.2;
 g_car_speed_mps_ki_min_max_impact = 5.0;
@@ -33,7 +33,7 @@ g_max_speed_after_delay_mps = 0.3;
 g_finish_line_angle_tolerance = 30.0;
 g_oversteer_mitigation_yaw_tolerance_rad_s = deg2rad(150);
 g_oversteer_mitigation_yaw_delta_tolerance_rad_s = deg2rad(360);
-g_edf_raw_speed = 180;
+g_edf_raw_speed = 150;
 
 g_powertrain_left_wheel_kp = 0.0;
 g_powertrain_left_wheel_ki = 0.1;
--- src/features/EDF.cpp
+++ src/features/EDF.cpp
@@ -5,10 +5,10 @@
 #include <PWMServo.h>
 #include "GlobalVariables.h"
 
-#define EDF_MIN_ACTIVE_TIME_S 0.5f
-#define EDF_STEERING_ANGLE_ACTIVATION_RAD radians(12.0f)
+#define EDF_MIN_ACTIVE_TIME_S 1.0f
+#define EDF_STEERING_ANGLE_ACTIVATION_RAD radians(0.0f)
 #define EDF_MIN_VEHICLE_SPEED_MPS 0.1f
-#define EDF_IDLE_RAW_SPEED 100
+#define EDF_IDLE_RAW_SPEED g_edf_raw_speed
 #define EDF_STANDSTILL_RAW_SPEED 90
 
 volatile PWMServo EDF_motor;
@@ -25,9 +25,9 @@ void EDF_setup(int edf_pin){
 void EDF_activation_loop(float steering_angle_rad){
     int8_t edf_vehicle_min_speed_achived = (int8_t)0;
     g_enable_edf = (int8_t)1;
-    if ((int)g_edf_raw_speed <= EDF_IDLE_RAW_SPEED) {
-        g_enable_edf = (int8_t)0;
-    }
+    //if ((int)g_edf_raw_speed <= EDF_IDLE_RAW_SPEED) {
+    //    g_enable_edf = (int8_t)0;
+    //}
     
     if (g_enable_car_engine != 0 && (floatCmp(g_car_speed_mps, EDF_MIN_VEHICLE_SPEED_MPS) >= 0) && (g_enable_edf != 0)) {
         if (local_EDF_active != (int8_t)0) {
